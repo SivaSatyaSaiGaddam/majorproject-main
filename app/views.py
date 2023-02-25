08e1c7b3db8df327431f79bfab176e4d4a631deb
SivaSatyaSaiGaddam <sivasatya604@gmail.com>
2023-02-25 19:48:36 +0530

    second commit in sub branch

--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,"home.html")
+
+def register(request):
+    return render(request,"register.html")
+
+def attendance(request):
+    return render(request,"attendnace.html")
+
+def show(request):
+    return render(request,"show.html")
